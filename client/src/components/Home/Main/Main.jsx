0108a1355bee59a1365770768eb74df07713cc25
Sam Berston <samuelberston@gmail.com>
2021-04-03 11:57:47 -0700

    setLoc prop types

--- client/src/components/Home/Main/Main.jsx
+++ client/src/components/Home/Main/Main.jsx
@@ -17,6 +17,7 @@ const Main = ({ view, displayedIssues, setLoc }) => (
 Main.propTypes = {
   view: PropTypes.string.isRequired,
   displayedIssues: PropTypes.arrayOf(PropTypes.object).isRequired,
+  setLoc: PropTypes.func.isRequired,
 };
 
 export default Main;
