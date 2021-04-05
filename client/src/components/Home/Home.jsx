2b581983a63978c65726633d2a9b5f001ce5b3ae
Sam Berston <samuelberston@gmail.com>
2021-04-05 11:22:01 -0700

    map render

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,40 +1,45 @@
-import React from 'react';
+import React from "react";
 // import axios from 'axios';
 
-import Header from './Header/Header.jsx';
-import LeftSideBar from './LeftSideBar/LeftSideBar.jsx';
-import RightSideBar from './RightSideBar/RightSideBar.jsx';
-import CreateIssue from './CreateIssue/CreateIssue.jsx';
-import Main from './Main/Main.jsx';
+import Header from "./Header/Header.jsx";
+import LeftSideBar from "./LeftSideBar/LeftSideBar.jsx";
+import RightSideBar from "./RightSideBar/RightSideBar.jsx";
+import CreateIssue from "./CreateIssue/CreateIssue.jsx";
+import Main from "./Main/Main.jsx";
 
 class Home extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       user: {
-        name: 'foo',
+        name: "foo",
       },
       location: {
         lat: 37.7749,
         lng: -122.4194,
       },
-      issues: [{
-        loc: {
-          lat: 100,
-          lng: 100,
+      issues: [
+        {
+          loc: {
+            lat: 100,
+            lng: 100,
+          },
         },
-      }],
-      categories: [
-        'theft',
-        'crime',
-        'for sale',
       ],
-      displayedIssues: [{
-        loc: {
-          lat: 37.7749,
-          lng: -122.4194,
+      categories: ["theft", "crime", "for sale"],
+      displayedIssues: [
+        {
+          name: "important issue",
+          description: "my car was stolen with all of my things in it",
+          photos: [
+            "https://magazine.northeast.aaa.com/wp-content/uploads/2017/10/how-to-report-a-stolen-car-1-640x423.jpg",
+          ],
+          loc: {
+            lat: 37.7749,
+            lng: -122.4194,
+          },
         },
-      }],
+      ],
       view: 0, // 0 = map view
     };
     this.getLoc = this.getLoc.bind(this);
@@ -49,7 +54,7 @@ class Home extends React.Component {
   getIssues() {
     // query database for the issues based on the user location
     this.setState({
-      issues: 'results of db query',
+      issues: "results of db query",
     });
   }
 
@@ -69,29 +74,22 @@ class Home extends React.Component {
   filterIssues() {
     // filtering algorithm
     this.setState({
-      displayedIssues: 'result of filtering algorithm',
+      displayedIssues: "result of filtering algorithm",
     });
   }
 
   render() {
-    const {
-      user, location, categories, displayedIssues, view,
-    } = this.state;
+    const { user, location, categories, displayedIssues, view } = this.state;
     return (
       <div id="homeContainer">
         <div id="home">
-          <Header
-            toggle={this.toggle}
-          />
+          <Header toggle={this.toggle} />
           <LeftSideBar
             user={user}
             categories={categories}
             filterIssues={this.filterIssues}
           />
-          <CreateIssue
-            user={user}
-            location={location}
-          />
+          <CreateIssue user={user} location={location} />
           <Main
             view={view}
             displayedIssues={displayedIssues}
