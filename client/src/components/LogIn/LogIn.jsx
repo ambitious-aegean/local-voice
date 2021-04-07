3dac8af7aec8310f646b32acbae762f001041b22
Ankur Tandan <tandan@gmail.com>
2021-04-07 10:18:08 -0700

    updated authentication

--- client/src/components/LogIn/LogIn.jsx
+++ client/src/components/LogIn/LogIn.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import LogOut from './LogOut.jsx';
+import Home from '../Home/Home.jsx';
 
 const LogIn = () => {
   const { user, isAuthenticated, isLoading, loginWithRedirect, logOut, username } = useAuth0();
@@ -19,6 +20,7 @@ const LogIn = () => {
             <h5>{user.name}</h5>
             <h5>{user.username}</h5>
             <img src={user.picture} alt={user.name} />
+            <Home />
           </div>
         )
       }
