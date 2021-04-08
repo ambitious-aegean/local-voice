886e5abd15bb1204ed7b42f3943d59475a94b9bf
gusfel <agustinjfeliciano@gmail.com>
2021-04-08 16:13:51 -0700

    did commentcard test and refactored checkVote to only query the db once when app renders

--- client/src/components/Home/Main/ListView/IssueCard/Discussion/CommentCard/CommentCard.test.js
+++ client/src/components/Home/Main/ListView/IssueCard/Discussion/CommentCard/CommentCard.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { configure, shallow, mount } from 'enzyme';
+import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import CommentCard from './CommentCard.jsx';
 
@@ -11,10 +11,6 @@ const user = { username: 'sampleUser123' };
 describe('testing the comment card', () => {
   const wrapper = shallow(<CommentCard comment={comment} user={user} />);
 
-  // it('should render two divs', () => {
-  //   const mainDiv = wrapper.find('#comment');
-  //   expect(mainDiv.find('#comment').children()).toHaveLength(2);
-  // });
   it('should render text correctly', () => {
     const mainDiv = wrapper.find('#comment');
     expect(mainDiv.find('#comment').childAt(0).text()).toEqual('sampleUser123');
