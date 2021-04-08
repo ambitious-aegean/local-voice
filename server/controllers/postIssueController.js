81aff303bd9c3c93be01554e2239c87d6fb0d644
Your Name <michellemhkang@gmail.com>
2021-04-08 12:48:26 -0700

    Fix post issue controller according to linter

--- server/controllers/postIssueController.js
+++ server/controllers/postIssueController.js
@@ -1,65 +1,65 @@
 const db = require('../../db/index.js');
 
 const postIssue = (req, res) => {
-    const {
-        categories, // array
-        title,
-        text,
-        lat,
-        lng,
-        photos, // array
-        rep_name,
-        // rep_title,
-        rep_email,
-        rep_photo_url,
-        date
-    } = req.body;
-    const issuesQuery = `INSERT INTO issues (title, text, lat, lng, rep_name, rep_email, rep_photo_url, date) VALUES ('${title}', '${text}', ${lat}, ${lng}, '${rep_name}', '${rep_email}', '${rep_photo_url}',' ${date}');`;
-    db.query(issuesQuery, (issueErr) => {
+  const {
+    categories, // array
+    title,
+    text,
+    lat,
+    lng,
+    photos, // array
+    rep_name,
+    // rep_title,
+    rep_email,
+    rep_photo_url,
+    date,
+  } = req.body;
+  const issuesQuery = `INSERT INTO issues (title, text, lat, lng, rep_name, rep_email, rep_photo_url, date) VALUES ('${title}', '${text}', ${lat}, ${lng}, '${rep_name}', '${rep_email}', '${rep_photo_url}',' ${date}');`;
+  db.query(issuesQuery, (issueErr) => {
     if (issueErr) {
-        console.log(issueErr);
-        res.status(400);  
+      console.log(issueErr);
+      res.status(400);
     } else {
-        const issueIdQuery = 'SELECT issue_id FROM issues ORDER BY issue_id DESC LIMIT 1;';
-        db.query(issueIdQuery, (issueIdErr, issueIdResults) => {
-            if (issueIdErr) {
-                console.log(issueIdErr);
+      const issueIdQuery = 'SELECT issue_id FROM issues ORDER BY issue_id DESC LIMIT 1;';
+      db.query(issueIdQuery, (issueIdErr, issueIdResults) => {
+        if (issueIdErr) {
+          console.log(issueIdErr);
+          res.status(400);
+        } else {
+          const issueId = issueIdResults[0].issue_id;
+          // console.log(issueId);
+          // console.log('issueId results:', issueIdResults);
+          Promise.all(categories.map((cat) => {
+            const categoriesQuery = `INSERT INTO issues_category (issue_id, cat_id) VALUES ('${issueId}', '${cat}');`;
+            return (db.query(categoriesQuery, (categoryErr) => {
+              if (categoryErr) {
+                console.log(categoryErr);
                 res.status(400);
-            } else {
-                const issueId = issueIdResults[0].issue_id;
-                // console.log(issueId);
-                // console.log('issueId results:', issueIdResults);
-                Promise.all(categories.map(cat => {
-                    const categoriesQuery = `INSERT INTO issues_category (issue_id, cat_id) VALUES ('${issueId}', '${cat}');`;
-                    return (db.query(categoriesQuery, (categoryErr) => {
-                        if (categoryErr) {
-                            console.log(categoryErr);
-                            res.status(400);
-                        }
-                    }))
-                }))
-                .then((response) => { 
-                    if (photos.length) {
-                        for (let i = 0; i < photos.length; i++) {
-                            const photosQuery = `INSERT INTO photos (issue_id, photo_info) VALUES ('${issueId}', '${photos[i]}');`
-                            db.query(photosQuery, (photoErr) => {
-                                if (photoErr) {
-                                    console.log(photoErr);
-                                    res.status(400);
-                                }
-                                if (i === photos.length - 1) {
-                                    res.sendStatus(200);
-                                }
-                            })
-                        }
-                    } else {
-                        res.sendStatus(200);
+              }
+            }));
+          }))
+            .then(() => {
+              if (photos.length) {
+                for (let i = 0; i < photos.length; i++) {
+                  const photosQuery = `INSERT INTO photos (issue_id, photo_info) VALUES ('${issueId}', '${photos[i]}');`;
+                  db.query(photosQuery, (photoErr) => {
+                    if (photoErr) {
+                      console.log(photoErr);
+                      res.status(400);
                     }
-                });
-            }
-        })
+                    if (i === photos.length - 1) {
+                      res.sendStatus(200);
+                    }
+                  });
+                }
+              } else {
+                res.sendStatus(200);
+              }
+            });
+        }
+      });
     }
-  })
+  });
 };
 
 module.exports.postIssue = postIssue;
