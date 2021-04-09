c2645b7904cd26b3d685405c0df69224456a8ea6
Sam Berston <samuelberston@gmail.com>
2021-04-08 17:52:51 -0700

    left side bar test

--- client/src/components/Home/LeftSideBar/LeftSideBar.test.jsx
+++ client/src/components/Home/LeftSideBar/LeftSideBar.test.jsx
@@ -5,6 +5,7 @@ import Enzyme, { shallow } from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16';
 
 import LeftSideBar from './LeftSideBar.jsx';
+import FilterOptions from './FilterOptions/FilterOptions.jsx';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
@@ -18,8 +19,14 @@ describe('test the left side bar', () => {
     }}
     filterIssues={() => {}}
   />);
+
   it('should exist', () => {
     const leftSideBar = wrapper.find('#LeftSideBar');
     expect(leftSideBar.exists()).toBe(true);
+  });
+
+  it('should display the filer options', () => {
+    const filterOptions = wrapper.find(FilterOptions);
+    expect(filterOptions.exists()).toBe(true);
   });
 });
