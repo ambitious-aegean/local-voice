b3445027e3a6e3d6fd1eb907ec233bb883f06d67
gusfel <agustinjfeliciano@gmail.com>
2021-04-10 11:58:37 -0700

    took out background color of isue card text

--- client/src/components/Home/Main/ListView/IssueCard/IssueCard.jsx
+++ client/src/components/Home/Main/ListView/IssueCard/IssueCard.jsx
@@ -204,9 +204,9 @@ class IssueCard extends React.Component {
           </div>
         </div>
         <div className={css.issueCardRight}>
-            <div role="button" className={css.dots} onClick={this.handleViewOptionsClick} onKeyPress={this.handleViewOptionsClick} tabIndex={0}>
-              <i className={`${css.dotsIcon} fa fa-ellipsis-h`} />
-            </div>
+          <div role="button" className={css.dots} onClick={this.handleViewOptionsClick} onKeyPress={this.handleViewOptionsClick} tabIndex={0}>
+            <i className={`${css.dotsIcon} fa fa-ellipsis-h`} />
+          </div>
         </div>
       </div>
     );
